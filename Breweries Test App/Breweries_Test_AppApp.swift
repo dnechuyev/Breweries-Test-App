4b0e0c2ad7792dba8a12b6feb19fe1a0be37ed39
Dmytro Nechuyev <dnechuyev@gmail.com>
2021-06-07 21:52:36 +0200

    Initial Commit

---
+++ Breweries Test App/Breweries_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breweries_Test_AppApp.swift
+//  Breweries Test App
+//
+//  Created by developer on 07.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Breweries_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Breweries Test App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Breweries Test App
+//
+//  Created by developer on 07.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
